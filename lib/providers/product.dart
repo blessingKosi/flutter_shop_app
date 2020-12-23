743c9dc3ac38e7482e1d0d2c7522a5537c0ad467
blessingKosi <kaniewu@gmail.com>
2020-12-23 23:10:47 +0000

    initial commit

---
+++ lib/providers/product.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import './authkey_url.dart';
+
+class Product with ChangeNotifier {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+  bool isFavorite;
+
+  Product({
+    @required this.description,
+    @required this.id,
+    @required this.imageUrl,
+    this.isFavorite = false,
+    @required this.price,
+    @required this.title,
+  });
+
+  void _setFavValue(bool newValue) {
+    isFavorite = newValue;
+    notifyListeners();
+  }
+
+  Future<void> toggleFavoriteStatus(String token, String userId) async {
+    final oldStatus = isFavorite;
+    isFavorite = !isFavorite;
+    notifyListeners();
+    final url = '$Firebase_Url/userFavorites/$userId/$id.json?auth=$token';
+    try {
+      final response = await http.put(
+        url,
+        body: json.encode(
+          isFavorite,
+        ),
+      );
+      if (response.statusCode >= 400) {
+        _setFavValue(oldStatus);
+      }
+    } catch (error) {
+      _setFavValue(oldStatus);
+    }
+  }
+}
